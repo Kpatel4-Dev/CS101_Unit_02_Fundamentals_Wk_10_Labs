a7130cc468eab0b62a77a1c7927687da58f139c1
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 19:55:06 -0700

    Create MadLibs.py

---
+++ ProgHW/MadLibs.py
@@ -0,0 +1,34 @@
+'''
+
+Lab 13 - Madlib
+
+Description: Using a List in Python, write a MadLib story, with at least 3-5 sentances.
+
+Developer Name(s): FIRST+LAST NAMES <EMAIL>,  ...
+
+Date: MM/DD/YYYY
+
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+
+
+
+
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+
+
+
